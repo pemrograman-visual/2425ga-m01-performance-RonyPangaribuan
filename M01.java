8be0d84e8c9a936be0408787e5efd02aab266a7b
rony <ronypangaribuan2505@gmail.com>
2024-10-18 10:14:41 +0700

    Telah Menyelesaikan M01

--- M01.java
+++ M01.java
@@ -1,2 +1,61 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24040 - Rony Reynaldy Pangaribuan
+// 12S24023 - Jaya Bestina Simbolon
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double nilaiAngka, kreditAngka, performa;
+        String kredit, nilaiHuruf;
+
+        kreditAngka = 0;
+        kredit = input.nextLine();
+        while (!kredit.equals("---")) {
+            if (kredit.equals("---")) {
+                kredit = kredit;
+            } else {
+                kreditAngka = Integer.parseInt(kredit);
+            }
+            nilaiHuruf = input.nextLine();
+            if (nilaiHuruf.equals("A")) {
+                nilaiAngka = 4.0;
+            } else {
+                if (nilaiHuruf.equals("AB")) {
+                    nilaiAngka = 3.5;
+                } else {
+                    if (nilaiHuruf.equals("B")) {
+                        nilaiAngka = 3.0;
+                    } else {
+                        if (nilaiHuruf.equals("BC")) {
+                            nilaiAngka = 2.5;
+                        } else {
+                            if (nilaiHuruf.equals("C")) {
+                                nilaiAngka = 2.0;
+                            } else {
+                                if (nilaiHuruf.equals("D")) {
+                                    nilaiAngka = 1.0;
+                                } else {
+                                    if (nilaiHuruf.equals("E")) {
+                                        nilaiAngka = 0.0;
+                                    } else {
+                                        nilaiAngka = 0.0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            performa = kreditAngka * nilaiAngka;
+            System.out.println(toFixed(performa,1));
+            kredit = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
